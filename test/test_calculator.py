f62877b95e7ee6af87dd13f9a109ff4def64ce2b
NyashaChiza <nchizampeni@gmail.com>
2023-12-20 13:18:42 +0200

    added example unit test

--- test/test_calculator.py
+++ test/test_calculator.py
@@ -5,5 +5,6 @@ from src import Calculator
 def calculator():
     return Calculator()
 
-
+def test_add_positive_numbers(calculator):
+    assert calculator.add(2, 3) == 5.0
 ##: Implement Your Unit Tests Here
